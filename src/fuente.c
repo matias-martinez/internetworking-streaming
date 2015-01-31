ac5b93ca98b6eecd9e56412bda568b83b01733dc
matias <martinezmatias@outlook.com>
2015-01-31 16:04:48 -0300

    .

---
+++ src/fuente.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "tcpDataStreaming.h"
+
+int main(){
+	
+	int sdf=connectTCP();
+	
+	//send () 
+}
